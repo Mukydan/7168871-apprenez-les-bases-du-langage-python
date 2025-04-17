33f1e7cc6c926219fda00f01106ed374657f08e2
Mukydan <143826847+Mukydan@users.noreply.github.com>
2025-04-17 12:18:59 +0000

    Update main.py

--- P1/P1C3/énoncé/main.py
+++ P1/P1C3/énoncé/main.py
@@ -1,1 +1,5 @@
-## Écrivez votre code ici !
+6. nom = "Mukenge"
+7. age = "18"
+8. print(f"Je m'appelle {nom} et j'ai {age} ans")
+9. age = age + 10
+10. print(f"Je m'appelle {nom} et j'ai {age} ans")
