371da0d7ff7e3e14bb0695f8cbe5b7285162deba
Danny Mukenge <mukydan@outlook.fr>
2025-04-26 13:11:02 +0000

    Les boucles

--- P2/P2C2/énoncé/main.py
+++ P2/P2C2/énoncé/main.py
@@ -1,1 +1,30 @@
-# Ecrivez votre code ici !
+# Ecrivez notre code ici !
+nombres = input('saississez une liste de nombres séparés par des virgules : ')
+print(nombres)
+# Utiliser la méthode split() pour transformer la variable nombres en une liste
+# de nombres
+liste = nombres.split(',')
+print(liste)
+# Transformer la liste en une liste d'entiers
+liste_entiers = []
+for i in range(len(liste)):
+    liste_entiers.append(int(liste[i]))
+print(liste_entiers)
+
+# Calculer et afficher la somme des nombres de la liste
+somme = 0
+for i in range(len(liste_entiers)):
+    somme += liste_entiers[i]
+print(f"La somme des nombres de la liste est : {somme} ")
+
+# Calculer et afficher la moyenne des nombres de la liste
+moyenne = somme  / len(liste_entiers)
+print(f"La moyenne des nombre de la liste est : {moyenne}")
+
+# Calculer et afficher le nombre de nombres dans la liste qui sont supérieurs
+# à la moyenne
+compteur = 0
+for i in range(len(liste_entiers)):
+    if liste_entiers[i] > moyenne:
+        compteur += 1
+print(f"le nombre de nombres supérieurs à la moyenne est : {compteur}")
--- P2/P2C3/énoncé/main.py
+++ P2/P2C3/énoncé/main.py
@@ -1,1 +1,33 @@
-# Ecrivez votre code ici
+# Vous devez créer un programme qui calcule le salaire horaire d'un employé, en fonction de son salaire mensuel et de son nombre 
+# d'heures travaillées par semaine
+
+# Calculez le salaire mensuel d'un employé à partir de son salaire annuel
+
+
+def salaire_mensuel(salaire_annuel):
+    salaire_mensuel = float(salaire_annuel) / 12
+    return salaire_mensuel
+
+# Calculez le salaire hebdomadaire d'un employé à partir de son salaire mensuel
+def salaire_hebdomadaire(salaire_mensuel):
+    salaire_hebdomadaire = float(salaire_mensuel) / 4
+    return salaire_hebdomadaire
+
+# Calculez le salaire horaire d'un employé à partir de son salaire hebdomadaire
+def salaire_horaire(salaire_hebdomadaire, heures_travaillees):
+    taux_horaire = float(salaire_hebdomadaire) / float(heures_travaillees)
+    return round(taux_horaire, 2)
+
+# Demander le salaire annuel
+salaire_annuel = input("Entrez le salaire annuel de l'employé : ")
+
+# Demander les heures travaillées par semaine
+heures_travaillees = input("Entrez votre volume d'heure de travaille par semaine : ")
+
+# Calcul du salaire horaire
+mon_salaire_mensuel = salaire_mensuel(salaire_annuel)
+mon_salaire_hebdomadaire = salaire_hebdomadaire(mon_salaire_mensuel)
+mon_salaire_horaire = salaire_horaire(mon_salaire_hebdomadaire, heures_travaillees)
+
+# Affichage du salaire horaire
+print(f"Votre salaire horaire est de : {mon_salaire_horaire} $ ")
